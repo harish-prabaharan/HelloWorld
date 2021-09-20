c4de825be3028bec638c9e0e77ff7c7e3b898f5e
harish-prabaharan <prabaharanharish@gmail.com>
2021-09-20 11:12:47 -0500

    adding imageView

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
